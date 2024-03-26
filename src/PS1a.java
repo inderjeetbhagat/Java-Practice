8f5f32a22b8eb4b67bf80fa5888104677746405f
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-03-27 04:49:00 +0530

    Practise Set 1 Started

---
+++ src/PS1a.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PS1a {
+    public static void main(String[] args) {
+        int a , b, c;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        c = a + b;
+        System.out.println("The Sum is c = " +c);
+    }
+}
